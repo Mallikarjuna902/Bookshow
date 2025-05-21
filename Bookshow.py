f448bbb8024e64d7520767b41db409c2a6faaf53
Mallikarjuna C <shivakumarbcgowda123@gmail.com>
2025-05-21 19:20:10 +0530

    Update Bookshow.py

--- Bookshow.py
+++ Bookshow.py
@@ -2,34 +2,37 @@ print("Theater Seats")
 H = int(input("Enter number of rows: "))
 L, M, R = 6, 10, 6
 T = L + M + R
-seats = [['A'] * T for _ in range(H)]
-for _ in range(H):
-    print('A' * L + '  ' + 'A' * M + '  ' + 'A' * R)
-for row in seats:
-    print(''.join(row[:L]) + '  ' + ''.join(row[L:L+M]) + '  ' + ''.join(row[L+M:]))
-row = int(input("Enter row number: "))
-col = int(input("Enter seat number: "))
-if 0 <= row < H and 0 <= col < T:
-    if seats[row][col] == 'A':
-        seats[row][col] = 'B'
-        print("Seat booked.")
-    else:
-        print("Seat already booked.")
-else:
-    print("Invalid seat.")
-for row in seats:
-    print(''.join(row[:L]) + '  ' + ''.join(row[L:L+M]) + '  ' + ''.join(row[L+M:]))
-if input("Book another? (yes/no): ").lower() == 'yes':
-    row = int(input("Enter row number: "))
-    col = int(input("Enter seat number: "))
-    if 0 <= row < H and 0 <= col < T:
-        if seats[row][col] == 'A':
-            seats[row][col] = 'B'
+seats = [['🟢'] * T for _ in range(H)]
+def show_seats():
+    print("\nAvailable = 🟢 | Booked = 🔴")
+    print("        L" + "   " * (L-1) + "M" + "  " * (M-1) + "R")
+    for i, row in enumerate(seats):
+        left = ''.join(row[:L])
+        mid = ''.join(row[L:L+M])
+        right = ''.join(row[L+M:])
+        print(f"{i:2d} {left}  {mid}  {right}")
+def get_index(section, num):
+    if section == 'L' and 1 <= num <= L: return num - 1
+    if section == 'M' and 1 <= num <= M: return L + num - 1
+    if section == 'R' and 1 <= num <= R: return L + M + num - 1
+    return -1
+def book():
+    r = int(input(f"Enter row (0–{H-1}): "))
+    s = input("Section (L/M/R): ").upper()
+    n = int(input("Seat number in section: "))
+    c = get_index(s, n)
+    if 0 <= r < H and c != -1:
+        if seats[r][c] == '🟢':
+            seats[r][c] = '🔴'
             print("Seat booked.")
         else:
             print("Seat already booked.")
     else:
-        print("Invalid seat.")
-print("Final Seat Layout:")
-for row in seats:
-    print(''.join(row[:L]) + '  ' + ''.join(row[L:L+M]) + '  ' + ''.join(row[L+M:]))
+        print("Invalid input.")
+show_seats()
+book()
+show_seats()
+if input("Book another? (yes/no): ").lower() == 'yes':
+    book(); show_seats()
+print("\nFinal Layout:")
+show_seats()
